dd3a14d173378aa6a290f4557a161316e043506c
Michael Boyer <mboy221@aucklanduni.ac.nz>
2026-09-02 14:20:55 +1200

    finished the parser for basic operations handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use crate::lexer::Lexer;
+use std::fs;
+
+use crate::{lexer::Lexer, parser::Parser};
 
 mod token;
 mod ast;
@@ -6,9 +8,11 @@ mod lexer;
 mod parser;
 
 fn main() {
-    let source: String = "var age = 128 + 35".to_string();
+    let source = fs::read_to_string("./test.cdm").expect("Failed to read the source file!");
     let lexer = Lexer::new(source);
     let tokens = lexer.scan_tokens();
-    let debug_string = format!("{:?}", tokens);
+    let parser = Parser::new(tokens);
+    let stmt = parser.parse();
+    let debug_string = format!("{:?}", stmt);
     println!("{}", debug_string);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,27 +1,97 @@
 #![allow(dead_code)]
 
+use core::panic;
+
 use crate::token::{Token, TokenType};
-use crate::ast::Expr; // Grab the Expr enum we defined earlier
+use crate::ast::{Expr, Stmt}; // Grab the Expr enum we defined earlier
 
 pub struct Parser {
     tokens: Vec<Token>,
+    statements: Vec<Stmt>,
     current: usize,
 }
 
 impl Parser {
     pub fn new(tokens: Vec<Token>) -> Self {
-        Self { tokens, current: 0 }
+        Self { tokens, current: 0, statements: Vec::new() }
     }
 
-    pub fn parse(&mut self) {
+    pub fn parse(mut self) -> Vec<Stmt> {
+        while !self.is_at_end() {
+            let peek = self.peek();
+            if peek.kind == TokenType::If
+                || peek.kind == TokenType::Var
+                || peek.kind == TokenType::Print {
+                let statement = self.statement();
+                self.statements.push(statement);
+            } else {
+                let expression = self.expression();
+                self.statements.push(Stmt::Expression(expression));
+            }
+        }
 
+        self.statements
     }
 
-    fn parse_token(&mut self) {
-        
+    fn statement(&mut self) -> Stmt {
+        let peek = self.peek();
+        self.advance();
+        match peek.kind {
+            TokenType::Print => {
+                let expr = self.expression();
+                Stmt::Print(expr)
+            }
+            TokenType::If => {
+                let expr = self.expression();
+                let then = self.statement();
+                Stmt::If { condition:expr, then_branch: Box::new(then), else_branch: None }
+            }
+            _ => {
+                let name = self.advance();
+                let equal = self.advance();
+                if equal.kind == TokenType::Equal {
+                    let expr = self.expression();
+                    return Stmt::Var { name: name.lexeme, initializer: expr };
+                }
+                panic!("Invalid variable declaration structure");
+            }
+        }
     }
 
-    // Inside impl Parser
+    fn expression(&mut self) -> Expr {
+        let mut expression = self.factor();
+        let mut peek = self.peek();
+        while peek.kind == TokenType::Plus
+            || peek.kind == TokenType::Minus {
+            let operator = self.advance();
+            let right = self.factor();
+            expression = Expr::Binary { 
+                left: Box::new(expression), 
+                operator, 
+                right: Box::new(right), 
+            };
+            peek = self.peek()
+        }
+        expression
+    }
+
+    fn factor(&mut self) -> Expr {
+        let mut expr = self.primary();
+        let mut peek = self.peek();
+        while peek.kind == TokenType::Star
+            || peek.kind == TokenType::Slash {
+            let operator = self.advance();
+            let right = self.primary();
+            expr = Expr::Binary { 
+                left: Box::new(expr), 
+                operator, 
+                right: Box::new(right), 
+            };
+            peek = self.peek()
+        }
+        expr
+    }
+
     fn primary(&mut self) -> Expr {
         let token = self.advance();
         match token.kind {
@@ -34,7 +104,7 @@ impl Parser {
     }
 
     fn is_at_end(&self) -> bool {
-        self.current >= self.tokens.len()
+        self.current >= self.tokens.len() || self.peek().kind == TokenType::EOF
     }
 
     fn peek(&self) -> Token {
